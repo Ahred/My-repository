f00e3f47d3a33dbe5e1d796dbfb6b120df99a7be
Ahred <53530503+Ahred@users.noreply.github.com>
2019-08-08 11:25:32 +0300

    Adding file

---
+++ PythonApplication3.py
@@ -0,0 +1,7 @@
+
+a=int(input('Enter first number''\n'))
+b=int(input('Enter first number''\n'))
+print("The result of summig your numbers:", a+b)
+print("The result of subtracting your numbers:", a-b)
+print("The result of multiplicating of your numbers:", a*b)
+print("The result of divising of your numbers:", a/b)
